9ff634b156068bbef505c14d62324c3a5e8c89a8
florian <florianbarjon@icloud.com>
2025-01-11 10:29:56 +0100

    side

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,9 +8,9 @@ import { Metadata } from "next";
 import Image from "next/image";
 
 export const metadata: Metadata = {
-  title: "Contact | John Doe",
+  title: "Contact | Florian Bajron",
   description:
-    "John Doe is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
+    "Florian Barjon est un développeur passionné, créatif et déterminé. Autiste Asperger, il transforme sa passion pour le code en projets uniques et impactants.",
 };
 
 export default function Projects() {
